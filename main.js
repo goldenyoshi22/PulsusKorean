acf13596076aa1ef7ca66d806e0ba34a58716274
goldenyoshi22 <59319965+goldenyoshi22@users.noreply.github.com>
2024-03-22 21:09:35 -0400

    mar-22-2024
    try hovering over some things

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ var difficultyColors = [
 ["#000000", "#666666"], //17
 ]
 
+var topPlayMults = [1.00, 0.98, 0.96, 0.94, 0.92, 0.90, 0.86, 0.82, 0.78, 0.74, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05]
+
 async function initAll() {
 	await sheetsToKids();
 	await sheetsToScores();
@@ -100,7 +102,8 @@ for (let i = 0; i < kidScores.length; i++) {
 				}
 			}
 			kidScores[i].scores[k].modMult = finalMult;
-			kidScores[i].scores[k].modInfo = finalString.slice(0, finalString.length - 2);
+			if (finalString == "") kidScores[i].scores[k].modInfo = "None";
+			else kidScores[i].scores[k].modInfo = finalString.slice(0, finalString.length - 2);
 		} else {
 			kidScores[i].scores[k].modMult = 1;
 			kidScores[i].scores[k].modInfo = "";
@@ -118,7 +121,6 @@ for (let i = 0; i < kidScores.length; i++) {
 		kidScores[i].scores.sort(function(a, b){return b.pulse - a.pulse});
 	};
 };
-	var topPlayMults = [1.00, 0.98, 0.96, 0.94, 0.92, 0.90, 0.86, 0.82, 0.78, 0.74, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05]
 	for (let i = 0; i < users.length; i++) {
 		users[i].scores = users[i].scores.sort(function(a, b){return b.pulse - a.pulse})
 		for (let k = 0; k < users[i].scores.length && k < topPlayMults.length; k++) {
@@ -176,13 +178,13 @@ function showUserProfile(targetUser) {
 	for (let i = 0; i < targetUser.scores.length; i++) {
 		let currentScore = targetUser.scores[i]
 		pendHTML += `
-		<tr>
+		<tbody><tr>
 		<td>${kids[currentScore.kid].name}</td>
 		<td style="${kids[currentScore.kid].difficulty >= 17 ? "font-style:italic;text-decoration:underline line-through;" : ""}background-color:${difficultyColors[Math.floor(Math.max(kids[currentScore.kid].difficulty, 0))][0]};color:${difficultyColors[Math.floor(Math.max(kids[currentScore.kid].difficulty, 0))][1]};">${kids[currentScore.kid].difficulty}</td>
-		<td>${(currentScore.accuracy * 100).toFixed(3)}%</td>
-		<td>${currentScore.pulse.toFixed(3)}p</td>
-		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "" : "(" + currentScore.modMult.toFixed(3) + "x)"}</td>
-		</tr>
+		<td title="Pulsus Accuracy: ${((currentScore.hits[0]*100+currentScore.hits[1]*100+currentScore.hits[2]*50+currentScore.hits[3]*20)/(kids[currentScore.kid].notes)).toFixed(3)}%" ${currentScore.hits[2]+currentScore.hits[3]+currentScore.hits[4] == 0 ? "style='background:-webkit-linear-gradient(left, #66CFFF, #DE66FF);-webkit-background-clip:text;-webkit-text-fill-color:transparent;'" : (currentScore.hits[4] == 0 ? "style='color:yellow;'" : "")}>${(currentScore.accuracy * 100).toFixed(3)}%</td>
+		<td title="Weighted: ${(currentScore.pulse * topPlayMults[i]).toFixed(3)}p">${currentScore.pulse.toFixed(3)}p</td>
+		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "None" : "(" + currentScore.modMult.toFixed(3) + "x)"}</td>
+		</tr></tbody>
 		`
 	}
 	
@@ -198,13 +200,13 @@ function showMapLeaderboard(mapID) {
 	for (let i = 0; i < kidScores[mapID].scores.length; i++) {
 		let currentScore = kidScores[mapID].scores[i];
 		pendHTML += `
-		<tr>
+		<tbody><tr>
 		<td>#${i+1}</td>
 		<td>${currentScore.username}</td>
-		<td>${(currentScore.accuracy * 100).toFixed(3)}%</td>
+		<td title="Pulsus Accuracy: ${((currentScore.hits[0]*100+currentScore.hits[1]*100+currentScore.hits[2]*50+currentScore.hits[3]*20)/(kids[currentScore.kid].notes)).toFixed(3)}%" ${currentScore.hits[2]+currentScore.hits[3]+currentScore.hits[4] == 0 ? "style='background:-webkit-linear-gradient(left, #66CFFF, #DE66FF);-webkit-background-clip:text;-webkit-text-fill-color:transparent;'" : (currentScore.hits[4] == 0 ? "style='color:yellow;'" : "")}>${(currentScore.accuracy * 100).toFixed(3)}%</td>
 		<td>${currentScore.pulse.toFixed(3)}p</td>
-		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "" : "(" + currentScore.modMult.toFixed(3) + "x)"}</td>
-		</tr>`
+		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "None" : "(" + currentScore.modMult.toFixed(3) + "x)"}</td>
+		</tr></tbody>`
 	}
 		
 	document.getElementById("mapLeaderboardText").innerHTML = `${kidScores[mapID].title} - Leaderboard`;
