e52f583ea8fc75eb6368086693c5a297ae7a1873
goldenyoshi22 <59319965+goldenyoshi22@users.noreply.github.com>
2024-06-18 19:10:04 -0400

    jun 18 2024
    code slight cleanup and hd is now 1.01x

--- leaderboards.js
+++ leaderboards.js
@@ -1,190 +1,7 @@
-var kidScores = [
-    {
-        "kid": 19,
-        "title": "You wouldn't steal a Hard",
-        "score0": "AFS/92,29,5,0,0"
-    },
-    {
-        "kid": 22,
-        "title": "Story of Undertale"
-    },
-    {
-        "kid": 5,
-        "title": "TECHNOPOLIS 2085",
-        "score0": "Sep/544,121,6,1,1"
-    },
-    {
-        "kid": 14,
-        "title": "Cannelés au Chocolat",
-        "score0": "goldenyoshi22/646,184,13,4,6",
-        "score1": "Lilyyy/779,67,6,0,1"
-    },
-    {
-        "kid": 15,
-        "title": "Izayoi Ravers",
-        "score0": "goldenyoshi22/738,118,46,3,0"
-    },
-    {
-        "kid": 16,
-        "title": "10 Things I Hate About You // Jaded",
-        "score0": "goldenyoshi22/488,183,28,2,3",
-        "score1": "methanal/652,52,0,0,0",
-        "score2": "AFS/468,159,61,11,5"
-    },
-    {
-        "kid": 2,
-        "title": "HELIX",
-        "score0": "Sep/497,124,19,2,2"
-    },
-    {
-        "kid": 20,
-        "title": "Start Again"
-    },
-    {
-        "kid": 21,
-        "title": "Happy Lucky->Injection!!"
-    },
-    {
-        "kid": 8,
-        "title": "Failure Girl [Passable]",
-        "score0": "Sep/945,141,8,0,0",
-        "score1": "methanal/990,100,3,1,0"
-    },
-    {
-        "kid": 13,
-        "title": "Corrupting Wonderland",
-        "score0": "Sep/747,202,31,0,2"
-    },
-    {
-        "kid": 11,
-        "title": "Made of Fire",
-        "score0": "Sep/499,104,25,0,4",
-        "score1": "methanal/575,55,2,0,0",
-        "score2": "AFS/352,169,76,14,21",
-        "score3": "goldenyoshi22/554,65,10,0,3"
-    },
-    {
-        "kid": 1,
-        "title": "Three eyes ~ awa...",
-        "score0": "Sep/938,216,30,6,6",
-        "score1": "methanal/953,206,39,6,8",
-        "score2": "goldenyoshi22/824,246,70,21,31"
-    },
-    {
-        "kid": 12,
-        "title": "How do you pronounce Boaz?",
-        "score0": "methanal/594,157,37,6,7"
-    },
-    {
-        "kid": 7,
-        "title": "Piercing Snowflake",
-        "score0": "methanal/429,85,6,0,2"
-    },
-    {
-        "kid": 23,
-        "title": "Crazy Frog",
-        "score0": "goldenyoshi22/1711,394,65,24,28",
-        "score1": "shianara/893,680,598,51,0",
-        "score2": "Lilyyy/918,762,373,49,120"
-    },
-    {
-        "kid": 9,
-        "title": "XNOR XNOR XNOR",
-        "score0": "Sep/2212,746,200,35,65"
-    },
-    {
-        "kid": 10,
-        "title": "true DJ MAG top ranker's song zenpen (katagiri remix)",
-        "score0": "goldenyoshi22/1977,763,295,34,71",
-        "score1": "methanal/2529,561,37,2,11"
-    },
-    {
-        "kid": 4,
-        "title": "Air",
-        "score0": "shianara/793,572,270,27,11"
-    },
-    {
-        "kid": 24,
-        "title": "Crystal Gravity"
-    },
-    {
-        "kid": 6,
-        "title": "FLAMEWALL // STELLAR CORE",
-        "score0": "methanal/2845,1000,145,10,40"
-    },
-    {
-        "kid": 17,
-        "title": "Ringo's Tea Party",
-        "score0": "methanal/1267,351,28,5,12",
-        "score1": "Lilyyy/953,394,188,42,86"
-    },
-    {
-        "kid": 3,
-        "title": "Apollo"
-    },
-    {
-        "kid": 0,
-        "title": "three eyes"
-    },
-    {
-        "kid": 18,
-        "title": "Circus Galop"
-    },
-    {
-        "kid": 25,
-        "title": "Mighty Little Man",
-        "score0": "AFS/78,9,0,0,0"
-    },
-    {
-        "kid": 26,
-        "title": "Bakuure! Match Uri no Haken Shoujo - Extra",
-        "score0": "AFS/601,334,83,13,14"
-    },
-    {
-        "kid": 27,
-        "title": "VULTURE",
-        "score0": "methanal/3178,810,120,11,26",
-        "score1": "AFS/1853,935,813,209,335"
-    },
-    {
-        "kid": 28,
-        "title": "najimi breakers",
-        "score0": "shianara/585,496,550,133,222"
-    },
-    {
-        "kid": 29,
-        "title": "Twinkle Parade"
-    },
-    {
-        "kid": 30,
-        "title": "Crazy Loop [Hyper]"
-    },
-    {
-        "kid": 31,
-        "title": "Cory in the House [Expert]"
-    },
-    {
-        "kid": 32,
-        "title": "EVERYBODY DO THE FLOP [EXPERT]"
-    },
-    {
-        "kid": 33,
-        "title": "ISSUE 480",
-        "score0": "shianara/713,464,92,3,0",
-        "score1": "methanal/1077,173,18,2,2",
-        "score2": "goldenyoshi22/913,274,47,6,32",
-        "score3": "Lilyyy/928,232,91,7,14"
-    },
-    {
-        "kid": 34,
-        "title": "Unplanned Pregnancy // CATASTROPHIC FAILURE",
-        "score0": "goldenyoshi22/231,95,21,4,0",
-        "score1": "methanal/290,56,4,0,1"
-    }
-]
+var kidScores = []; //scores on maps
+var users = []; //player profiles
 
-var users = []
-
+//gets user scores from google sheet
 function sheetsToScores() {
 	return new Promise((resolve, reject) => {
 	kidScores = [];
@@ -195,7 +12,7 @@ function sheetsToScores() {
 			kidScores.push({
 				"kid": parseInt(data.values[i][0]),
 				"title": data.values[i][1],
-			})
+			});
 			for (let k = 2; k < data.values[i].length; k++) {
 			kidScores[kidScores.length - 1][`score${k - 2}`] = data.values[i][k];
 			}
@@ -206,6 +23,11 @@ function sheetsToScores() {
 	});
 }
 
+//calculate either pulse or acc
+/*
+kid = koreanID, hits = array for judgements [marv,great,good,ok,miss],
+type = "pulse" for pulse or "accuracy" for accuracy%, forceAcc = force accuracy% for hypothetical plays
+*/
 function calculate(kid, hits, type = "pulse", forceAcc = false) {
 	let diff = kids[kid].difficulty;
 	let notes = (typeof kids[kid].notes == "number" && !isNaN(kids[kid].notes)) ? kids[kid].notes : hits[0]+hits[1]+hits[2]+hits[3]+hits[4]
@@ -221,39 +43,6 @@ function calculate(kid, hits, type = "pulse", forceAcc = false) {
 		return acc;
 		break;
 	}
+	//this below is severely ourdated dont worry about it lol
 	//KP = ((5 * ((1.4 ^ diff) * (min(notes,3737) ^ 0.1))) ^ (acc/100)) * min(notes/500, 1)
-}
-
-async function initLeaderboards() {
-	await sheetsToKids();
-	await sheetsToScores();
-for (let i = 0; i < kidScores.length; i++) {
-	kidScores[i].scores = [];
-	for (let k = 0; kidScores[i]["score" + k] != undefined; k++) {
-		console.log(kidScores);
-		kidScores[i].scores.push({username: kidScores[i]["score" + k].split("/")[0], hits: kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)}), kid: kidScores[i].kid,
-		pulse: calculate(i, kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)})),
-		accuracy: calculate(i, kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)}), "accuracy")})
-		let userNames = [];
-		for (let m = 0; m < users.length; m++) {
-			userNames.push(users[m].username);
-		};
-		if (!userNames.includes(kidScores[i].scores[k].username)) {
-			users.push({username: kidScores[i].scores[k].username, scores: [], pulse: 0});
-		};
-		users[users.findIndex(item => item.username === kidScores[i].scores[k].username)].scores.push(kidScores[i].scores[k])
-	};
-};
-}
-
-async function initUsers() {
-	await sheetsToKids();
-	var topPlayMults = [1.00, 0.98, 0.96, 0.94, 0.92, 0.90, 0.86, 0.82, 0.78, 0.74, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05]
-	for (let i = 0; i < users.length; i++) {
-		users[i].scores = users[i].scores.sort(function(a, b){return calculate(b.kid, b.hits) - calculate(a.kid, a.hits)})
-		for (let k = 0; k < users[i].scores.length && k < topPlayMults.length; k++) {
-			users[i].pulse += users[i].scores[k].pulse * topPlayMults[k];
-		}
-	}
-	users = users.sort(function(a, b){return b.pulse - a.pulse})
 }
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var difficultyColors = [
+["#000000", "#0000ff"], //-1, replaced with "?" and used as a placeholder diff if a map is either a complete joke or not currently rated in difficulty
 ["#4a86e8", "#000000"], //0
 ["#00ffff", "#000000"], //1
 ["#00ff00", "#000000"], //2
@@ -18,7 +19,7 @@ var difficultyColors = [
 ["#000000", "#ff0000"], //16
 ["#000000", "#666666"], //17
 ]
-
+//best play is 100% pulse, 2nd best play is 98% pulse, etc.
 var topPlayMults = [1.00, 0.98, 0.96, 0.94, 0.92, 0.90, 0.86, 0.82, 0.78, 0.74, 0.70, 0.65, 0.60, 0.55, 0.50, 0.45, 0.40, 0.35, 0.30, 0.25, 0.20, 0.15, 0.10, 0.05]
 
 async function initAll() {
@@ -26,102 +27,105 @@ async function initAll() {
 	await sheetsToScores();
 	sortKids("difficulty");
 	initAwardedMaps();
-for (let i = 0; i < kidScores.length; i++) {
-	kidScores[i].scores = [];
-	for (let k = 0; kidScores[i]["score" + k] != undefined; k++) {
-		kidScores[i].scores.push({username: kidScores[i]["score" + k].split("/")[0], mods: kidScores[i]["score" + k].split("/")[2] == undefined ? undefined : kidScores[i]["score" + k].split("/")[2].split(","), hits: kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)}), kid: kidScores[i].kid,
-		pulse: calculate(i, kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)})),
-		accuracy: calculate(i, kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)}), "accuracy")})
-		
-		//mods
-		/*
-		bpm:
-		pulse*((5 ^ bpm)/5) if <1x
-		pulse*bpm if >=1x
+	//init map leaderboards/scores
+	for (let i = 0; i < kidScores.length; i++) {
+		kidScores[i].scores = [];
+		for (let k = 0; kidScores[i]["score" + k] != undefined; k++) {
+			//pushing a score based on the columns from the google sheet, hence the ["score" + k] because the columns are named like that
+			//this next line is barely readable but shh
+			kidScores[i].scores.push({username: kidScores[i]["score" + k].split("/")[0], mods: kidScores[i]["score" + k].split("/")[2] == undefined ? undefined : kidScores[i]["score" + k].split("/")[2].split(","), hits: kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)}), kid: kidScores[i].kid,
+			pulse: calculate(i, kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)})),
+			accuracy: calculate(i, kidScores[i]["score" + k].split("/")[1].split(",").map(function(item) {return parseFloat(item)}), "accuracy")})
+			
+			//mods
+			/*
+			bpm:
+			pulse^bpm if <1x
+			pulse*bpm if >=1x
 
-		fs:
-		if 0.8x or less, 1.03x
-		if 0.5x or less, 1.06x
+			fs:
+			if 0.8x or less, 1.03x
+			if 0.5x or less, 1.06x
 
-		hw:
-		pulse / hw if hw > 1
-		pulse / (hw^0.5) if hw <= 1
+			hw:
+			pulse / hw if hw > 1
+			pulse / (hw^0.5) if hw <= 1
 
-		at: nuh uh
-		nf: 0.8x
-		nr: 0.8x
-		hd: 1.02x
-		fl: 1.1x
-		rd: Math.random() * 1.2 (jk)
-		*/
-		if (kidScores[i].scores[k].mods != undefined) {
-			finalMult = 1;
-			finalExponent = 1;
-			finalString = "";
-			//console.log(kidScores[i].scores[k]);
-				let modArray = kidScores[i].scores[k].mods;
-			for (let modIndex = 0; modIndex < modArray.length; modIndex++) {
-				if (modArray[modIndex].startsWith("bpm")) {
-					let bpmMod = modArray[modIndex].slice(3);
-					if (bpmMod >= 1) finalMult *= bpmMod;
-					//else finalMult *= ((25 ** bpmMod) / 25);
-					else finalExponent = 1 - (1 - bpmMod)/2;
-					finalString += `BPM ${bpmMod}x, `
+			at: nuh uh
+			nf: 0.8x
+			nr: 0.8x
+			hd: 1.01x
+			fl: 1.1x
+			rd: Math.random() * 1.2 (jk)
+			*/
+			if (kidScores[i].scores[k].mods != undefined) {
+				finalMult = 1;
+				finalExponent = 1;
+				finalString = "";
+				//console.log(kidScores[i].scores[k]);
+					let modArray = kidScores[i].scores[k].mods;
+				for (let modIndex = 0; modIndex < modArray.length; modIndex++) {
+					if (modArray[modIndex].startsWith("bpm")) {
+						let bpmMod = modArray[modIndex].slice(3);
+						if (bpmMod >= 1) finalMult *= bpmMod;
+						//else finalMult *= ((5 ** bpmMod) / 5);
+						else finalExponent = 1 - (1 - bpmMod)/2;
+						finalString += `BPM ${bpmMod}x, `
+					}
+					if (modArray[modIndex].startsWith("fs")) {
+						let fsMod = modArray[modIndex].slice(2);
+						if (fsMod <= 0.5) finalMult *= 1.06;
+						else if (fsMod <= 0.8) finalMult *= 1.03;
+						finalString += `FS ${fsMod}x, `
+					}
+					if (modArray[modIndex].startsWith("hw")) {
+						let hwMod = modArray[modIndex].slice(2);
+						if (hwMod > 1) finalMult /= hwMod;
+						else finalMult /= (hwMod ** 0.5);
+						finalString += `HW ${hwMod}x, `
+					}
+					if (modArray[modIndex] == "nf") {
+						finalMult *= 0.8;
+						finalString += `NF, `
+					}
+					if (modArray[modIndex] == "nr") {
+						finalMult *= 0.8;
+						finalString += `NR, `
+					}
+					if (modArray[modIndex] == "hd") {
+						finalMult *= 1.01;
+						finalString += `HD, `
+					}
+					if (modArray[modIndex] == "fl") {
+						finalMult *= 1.1;
+						finalString += `FL, `
+					}
 				}
-				if (modArray[modIndex].startsWith("fs")) {
-					let fsMod = modArray[modIndex].slice(2);
-					if (fsMod <= 0.5) finalMult *= 1.06;
-					else if (fsMod <= 0.8) finalMult *= 1.03;
-					finalString += `FS ${fsMod}x, `
-				}
-				if (modArray[modIndex].startsWith("hw")) {
-					let hwMod = modArray[modIndex].slice(2);
-					if (hwMod > 1) finalMult /= hwMod;
-					else finalMult /= (hwMod ** 0.5);
-					finalString += `HW ${hwMod}x, `
-				}
-				if (modArray[modIndex] == "nf") {
-					finalMult *= 0.8;
-					finalString += `NF, `
-				}
-				if (modArray[modIndex] == "nr") {
-					finalMult *= 0.8;
-					finalString += `NR, `
-				}
-				if (modArray[modIndex] == "hd") {
-					finalMult *= 1.02;
-					finalString += `HD, `
-				}
-				if (modArray[modIndex] == "fl") {
-					finalMult *= 1.1;
-					finalString += `FL, `
-				}
+				kidScores[i].scores[k].modMult = finalMult;
+				kidScores[i].scores[k].modExponent = finalExponent;
+				if (finalString == "") kidScores[i].scores[k].modInfo = "None";
+				else kidScores[i].scores[k].modInfo = finalString.slice(0, finalString.length - 2);
+			} else {
+				kidScores[i].scores[k].modMult = 1;
+				kidScores[i].scores[k].modExponent = 1;
+				kidScores[i].scores[k].modInfo = "";
 			}
-			kidScores[i].scores[k].modMult = finalMult;
-			kidScores[i].scores[k].modExponent = finalExponent;
-			if (finalString == "") kidScores[i].scores[k].modInfo = "None";
-			else kidScores[i].scores[k].modInfo = finalString.slice(0, finalString.length - 2);
-		} else {
-			kidScores[i].scores[k].modMult = 1;
-			kidScores[i].scores[k].modExponent = 1;
-			kidScores[i].scores[k].modInfo = "";
-		}
-		kidScores[i].scores[k].pulse *= kidScores[i].scores[k].modMult;
-		kidScores[i].scores[k].pulse **= kidScores[i].scores[k].modExponent;
-		
-		let userNames = [];
-		for (let m = 0; m < users.length; m++) {
-			userNames.push(users[m].username);
+			kidScores[i].scores[k].pulse *= kidScores[i].scores[k].modMult;
+			kidScores[i].scores[k].pulse **= kidScores[i].scores[k].modExponent;
+			
+			let userNames = [];
+			for (let m = 0; m < users.length; m++) {
+				userNames.push(users[m].username);
+			};
+			if (!userNames.includes(kidScores[i].scores[k].username)) {
+				users.push({username: kidScores[i].scores[k].username, scores: [], pulse: 0});
+			};
+			users[users.findIndex(item => item.username === kidScores[i].scores[k].username)].scores.push(kidScores[i].scores[k]);
+			kidScores[i].scores.sort(function(a, b){return b.pulse - a.pulse});
 		};
-		if (!userNames.includes(kidScores[i].scores[k].username)) {
-			users.push({username: kidScores[i].scores[k].username, scores: [], pulse: 0});
-		};
-		users[users.findIndex(item => item.username === kidScores[i].scores[k].username)].scores.push(kidScores[i].scores[k]);
-		kidScores[i].scores.sort(function(a, b){return b.pulse - a.pulse});
 	};
-};
 	for (let i = 0; i < users.length; i++) {
-		users[i].scores = users[i].scores.sort(function(a, b){return b.pulse - a.pulse})
+		users[i].scores = users[i].scores.sort(function(a, b){return b.pulse - a.pulse});
 		for (let k = 0; k < users[i].scores.length && k < topPlayMults.length; k++) {
 			users[i].pulse += users[i].scores[k].pulse * topPlayMults[k];
 		}
@@ -138,18 +142,18 @@ for (let i = 0; i < kidScores.length; i++) {
 }
 
 function initAwardedMaps() {
-	document.getElementById("awardedMapsTable").innerHTML = `<tr><th>ID</th><th>Title</th><th>Author</th><th>Difficulty</th><th>Skillset</th><th>Notes</th></tr>`
+	document.getElementById("awardedMapsTable").innerHTML = `<tr><th>ID</th><th>Title</th><th>Author</th><th>Difficulty</th><th>Skillset</th><th>Notes</th></tr>`;
 	for (let i = 0; i < sortedKids.length; i++) {
 		document.getElementById("awardedMapsTable").innerHTML += `<tr><td title="KID: ${sortedKids[i].kid}">${sortedKids[i].id}</td><td style="cursor:pointer;" onclick="showMapLeaderboard(${i})"><b>${sortedKids[i].name}</b></td><td>${sortedKids[i].author}</td>
-		<td style="${sortedKids[i].difficulty >= 17 ? "font-style:italic;text-decoration:underline line-through;" : ""}background-color:${difficultyColors[Math.floor(Math.max(sortedKids[i].difficulty, 0))][0]};color:${difficultyColors[Math.floor(Math.max(sortedKids[i].difficulty, 0))][1]};">${sortedKids[i].difficulty}</td>
+		<td style="${sortedKids[i].difficulty >= 17 ? "font-style:italic;text-decoration:underline line-through;" : ""}background-color:${difficultyColors[Math.floor(Math.max(sortedKids[i].difficulty + 1, 0))][0]};color:${difficultyColors[Math.floor(Math.max(sortedKids[i].difficulty + 1, 0))][1]};">${sortedKids[i].difficulty >= 0 ? sortedKids[i].difficulty : "?"}</td>
 		<td>${sortedKids[i].skill}</td>
-		<td>${isNaN(sortedKids[i].notes) ? "?" : sortedKids[i].notes}</td></tr>`
-	}
+		<td>${isNaN(sortedKids[i].notes) ? "?" : sortedKids[i].notes}</td></tr>`;
+	};
 }
 
 function initUserDisplay() {
 	for (let i = 0; i < users.length; i++) {
-		document.getElementById("leaderboardTable").innerHTML += `<tr><td>#${i + 1}</td><td><u style="cursor:pointer;" onclick='showUserProfile(${JSON.stringify(users[i])})'>${users[i].username}</u></td><td>${users[i].pulse.toFixed(3)}p</td>
+		document.getElementById("leaderboardTable").innerHTML += `<tr><td>#${i + 1}</td><td><u style="cursor:pointer;" onclick='showUserProfile(${JSON.stringify(users[i])})'>${users[i].username}</u></td><td>${Math.round(users[i].pulse * 1000) / 1000}p</td>
 		<td>${kids[users[i].scores[0].kid].name} ${users[i].scores[0].modInfo == "" ? "" : "[" + users[i].scores[0].modInfo + "]"} ~ ${users[i].scores[0].pulse.toFixed(3)}p (${(users[i].scores[0].accuracy * 100).toFixed(3)}%)</td></tr>`
 	};
 }
@@ -170,22 +174,22 @@ function showUserProfile(targetUser) {
 	document.getElementById("profileInfo").innerHTML = `
 	<h3>${targetUser.username} - #${targetUser.rank}</h3>
 	Pulse: ${typeof targetUser.pulse != "number" ? "???" : targetUser.pulse.toFixed(3)}p
-	`
+	`;
 	
-	let pendHTML = `<table><tr><th>Map</th><th>Difficulty</th><th>Accuracy</th><th>Pulse</th><th>Mods</th></tr>`
+	let pendHTML = `<table><tr><th>Map</th><th>Difficulty</th><th>Accuracy</th><th>Pulse</th><th>Mods</th></tr>`;
 	
 	for (let i = 0; i < targetUser.scores.length; i++) {
 		let currentScore = targetUser.scores[i]
 		pendHTML += `
 		<tbody><tr>
 		<td>${kids[currentScore.kid].name}</td>
-		<td style="${kids[currentScore.kid].difficulty >= 17 ? "font-style:italic;text-decoration:underline line-through;" : ""}background-color:${difficultyColors[Math.floor(Math.max(kids[currentScore.kid].difficulty, 0))][0]};color:${difficultyColors[Math.floor(Math.max(kids[currentScore.kid].difficulty, 0))][1]};">${kids[currentScore.kid].difficulty}</td>
+		<td style="${kids[currentScore.kid].difficulty >= 17 ? "font-style:italic;text-decoration:underline line-through;" : ""}background-color:${difficultyColors[Math.floor(Math.max(kids[currentScore.kid].difficulty + 1, 0))][0]};color:${difficultyColors[Math.floor(Math.max(kids[currentScore.kid].difficulty + 1, 0))][1]};">${kids[currentScore.kid].difficulty >= 0 ? kids[currentScore.kid].difficulty : "?"}</td>
 		<td title="Pulsus Accuracy: ${((currentScore.hits[0]*100+currentScore.hits[1]*100+currentScore.hits[2]*50+currentScore.hits[3]*20)/(kids[currentScore.kid].notes)).toFixed(3)}%" ${currentScore.hits[2]+currentScore.hits[3]+currentScore.hits[4] == 0 ? "style='background:-webkit-linear-gradient(left, #66CFFF, #DE66FF);-webkit-background-clip:text;-webkit-text-fill-color:transparent;'" : (currentScore.hits[4] == 0 ? "style='color:yellow;'" : "")}>${(currentScore.accuracy * 100).toFixed(3)}%</td>
 		<td title="Weighted: ${(currentScore.pulse * topPlayMults[i]).toFixed(3)}p">${currentScore.pulse.toFixed(3)}p</td>
-		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "None" : "(" + currentScore.modMult.toFixed(3) + "x)<sup>" + (currentScore.modExponent == 1 ? "" : currentScore.modExponent.toFixed(3)) + "</sup>"}</td>
+		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "None" : "(" + Math.round(currentScore.modMult * 1000)/1000 + "x)<sup>" + (currentScore.modExponent == 1 ? "" : Math.round(currentScore.modExponent * 1000)/1000) + "</sup>"}</td>
 		</tr></tbody>
 		`
-	}
+	};
 	
 	document.getElementById("profileScores").innerHTML = `${pendHTML}</table>`;
 	document.getElementById("userProfileText").innerHTML = `${targetUser.username}'s Profile`;
@@ -204,9 +208,9 @@ function showMapLeaderboard(mapID) {
 		<td>${currentScore.username}</td>
 		<td title="Pulsus Accuracy: ${((currentScore.hits[0]*100+currentScore.hits[1]*100+currentScore.hits[2]*50+currentScore.hits[3]*20)/(kids[currentScore.kid].notes)).toFixed(3)}%" ${currentScore.hits[2]+currentScore.hits[3]+currentScore.hits[4] == 0 ? "style='background:-webkit-linear-gradient(left, #66CFFF, #DE66FF);-webkit-background-clip:text;-webkit-text-fill-color:transparent;'" : (currentScore.hits[4] == 0 ? "style='color:yellow;'" : "")}>${(currentScore.accuracy * 100).toFixed(3)}%</td>
 		<td>${currentScore.pulse.toFixed(3)}p</td>
-		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "None" : "(" + currentScore.modMult.toFixed(3) + "x)<sup>" + (currentScore.modExponent == 1 ? "" : currentScore.modExponent.toFixed(3)) + "</sup>"}</td>
+		<td>${currentScore.modInfo} ${currentScore.modInfo == "" ? "None" : "(" + Math.round(currentScore.modMult * 1000)/1000 + "x)<sup>" + (currentScore.modExponent == 1 ? "" : Math.round(currentScore.modExponent * 1000)/1000) + "</sup>"}</td>
 		</tr></tbody>`
-	}
+	};
 		
 	document.getElementById("mapLeaderboardText").innerHTML = `${kidScores[mapID].title} - Leaderboard`;
 	document.getElementById("mapLeaderboardTable").innerHTML = pendHTML;
